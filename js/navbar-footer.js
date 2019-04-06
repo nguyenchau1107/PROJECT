886c63b40d00b2a0fd00b436c7597bf3f8c18147
nguyenchau1107 <nguyenchau110700@gmail.com>
2019-04-06 19:24:16 +0700

    create project

---
+++ js/navbar-footer.js
@@ -0,0 +1,35 @@
+//    <-!Navbar->
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (window.screen.width > 1000) {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+            document.getElementById("nav1").style.padding = "0 0";
+            document.getElementById("logo").style.width = "100px";
+            document.getElementById("logo").style.marginTop = "-10px";
+            document.getElementById("logo").style.marginLeft = "90%";
+            document.getElementById("nav1").style.backgroundColor = "rgba(255,152,76,0.57)";
+            document.getElementById("dropdown-menu-scroll-1").style.backgroundColor = "rgba(255,152,76,0.57)";
+            document.getElementById("dropdown-menu-scroll-2").style.backgroundColor = "rgba(255,152,76,0.57)";
+            document.getElementById("dropdown-menu-scroll-3").style.backgroundColor = "rgba(255,152,76,0.57)";
+        } else {
+            document.getElementById("nav1").style.padding = "60px 0";
+            document.getElementById("logo").style.width = "300px";
+            document.getElementById("logo").style.marginTop = "-40px";
+            document.getElementById("logo").style.marginLeft = "0";
+            document.getElementById("nav1").style.backgroundColor = "rgba(255,152,76,0.57)";
+            document.getElementById("dropdown-menu-scroll-1").style.backgroundColor = "rgba(255,152,76,0.57)";
+            document.getElementById("dropdown-menu-scroll-2").style.backgroundColor = "rgba(255,152,76,0.57)";
+            document.getElementById("dropdown-menu-scroll-3").style.backgroundColor = "rgba(255,152,76,0.57)";
+        }
+    }
+}
+
+            function scrollToTop() {
+                verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+                element = $('body');
+                offset = element.offset();
+                offsetTop = offset.top;
+                $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+        };
+
+
